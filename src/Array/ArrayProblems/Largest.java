72cd82024e527b6f32f42fec1966513973be6554
Shubham Yadav <imshubh19@gmail.com>
2024-08-20 16:10:47 +0530

    Solved Second largest Problem

--- src/Array/ArrayProblems/Largest.java
+++ src/Array/ArrayProblems/Largest.java
@@ -1,7 +1,5 @@
 package Array.ArrayProblems;
 
-import java.util.Arrays;
-
 
 
 /*public class Largest {	
@@ -15,14 +13,22 @@ import java.util.Arrays;
 	}
 */
 
-public class Largest{
+/*public class Largest{
 	static int largest(int arr[], int n){
 		Arrays.sort(arr);           //Brute force approach sort the array with built in function & return last value
 		int result=arr[n-1];       //as after sorting largest element will be at last;
 		return result;
+	}     
+		*/
+
+public class Largest{
+	static int largest(int arr[], int n){
+		int result=Integer.MIN_VALUE;         //Initilize the min value as largest
+		for(int i=0; i<n; i++){               // treverse the whole array
+			result=Math.max(result,arr[i]);      // using built in function to get largest value update the result
+		}
+		return result;
 	}
-
-
 	public static void main(String[] args) {
 		
 		int arr[]={3,3,6,1};
--- src/Array/ArrayProblems/SecondLargest.java
+++ src/Array/ArrayProblems/SecondLargest.java
@@ -1,38 +1,41 @@
 package Array.ArrayProblems;
 
 
-public class SecondLargest {
+/*public class SecondLargest {
 	static int secondlargest(int arr[], int n) {
-		//brute force approach 
-		//sort the array and return the n-2 
-		//Arrays.sort(arr);
-		//return arr[n-2];
-	
-	
-	// the upper brute force approach will fail if the element of n-1 & n-2 will be same
-	//another approch to solve this problem will be
-	
-	//sort the arr
+	         Arrays.sort(arr);          //sorts the array throgh built in function
+             int largest=arr[n-1];       //after sorting store the largest element as arr[n-1]
+             int slargest=-1;            //initilize secondlargest as -1 worst case if duplicates are present
+             for(int i=n-2; i>=0; i--){   //traverse the array from backwards i.e i=n-2 and compare with largest
+                if(arr[i]<largest){
+                    slargest=arr[i];       //if arr[i]<largest update slargest & break the loop
+                    break;
+                }                               //TC of this is O(nlogn) for sorting & O(n) for single traversal 
+                                                // TC is O(nlogn)
+             }
+             return slargest;
+       
+        }          
+             */
 
-        int largest = 0;
-        int slargest = -1;
-        
-        // Find the largest element
-        for (int i = 0; i < n; i++) {
-            if (arr[i] > largest) {
-                slargest = largest;
-                largest = arr[i];
-            } else if (arr[i] > slargest && arr[i] != largest) {
-                slargest = arr[i];
-            }
+             class SecondLargest{
+                static int secondlargest(int arr[], int n){   //This is more optimized approach
+                    int largest=arr[0];                        //store the first element as largest 
+                    int slargest=-1;                       //initize slargest as -1
+                 for(int i=1; i<n; i++){                 //traverse the whole arr
+                    if(arr[i]>largest){                 //compare largest with arr[i] & if arr[i] is larger than largest
+                        slargest=largest;                // update the slargest as largest
+                        largest=arr[i];                 //& largest as arr[i] 
+                    }
+                    if(arr[i]>slargest && arr[i]<largest){            //suppose there is element which is smaller than largest
+                        slargest=arr[i];                            //but greator than second largest than update that element as slargest
+                    }
+                 }
+                 return slargest;
         }
-        
-        return slargest;
-    }
-    
-    public static void main(String[] args) {
-        
-        int arr[] = {10, 5, 8, 20};
+        public static void main(String[] args) {
+       int arr[] = {10, 5, 8, 20};
+      //  int arr[]={10,10,10,10,10};
         int n = arr.length;
         System.out.println(secondlargest(arr, n));
 	}
