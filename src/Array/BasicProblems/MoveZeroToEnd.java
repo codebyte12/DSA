46ac06365d182d14b2f7e63fcddbe672b1c542bf
Shubham Yadav <imshubh19@gmail.com>
2024-08-22 01:12:56 +0530

    Optimized code

--- src/Array/BasicProblems/MoveZeroToEnd.java
+++ src/Array/BasicProblems/MoveZeroToEnd.java
@@ -1,5 +1,6 @@
 package Array.BasicProblems;
 
+//Brute Force Approach
 public class MoveZeroToEnd {
     static void moveZero(int arr[], int n){
         int temp[]=new int[n];                   //creating a temp array to store non zero elements of array
@@ -8,13 +9,20 @@ public class MoveZeroToEnd {
             if(arr[i]!= 0)
                 temp[index++] = arr[i];            // non zero elements stored
             }
-            for(int i=0; i<index; i++){
-                arr[i]=temp[i];                      //
+            for(int i=0; i<index; i++){             //Running a loop from 0--index
+                arr[i]=temp[i];                      //to copy temp arr to original arr
               }
-            for(int i=index; i<n; i++){
-                arr[i]=0;
+            for(int i=index; i<n; i++){          //traversing once again from index-n
+                arr[i]=0;                      // to place zeroes ata last
             }
     }
+
+    //The time complexcity of this will be (i) first loop-O(n)
+                                         //(ii)sexond loop-O(x)   suppose there are x no of non zero elements
+                                         //(iii)third loop-O(n-x)
+                                         // TC=O(n)+O(x)+O(n-x)=O(2n)
+
+   //Space complexcity is O(n) as we are using temp arr of size n
 
     public static void main(String[] args){
         int arr[]={1,0,2,3,2,0,0,4,5,6};
