7b43af88163a98f64c7dc956237e2844ff401f2b
Shubham Yadav <imshubh19@gmail.com>
2024-08-23 13:41:27 +0530

    Solved Remove Duplicates

--- src/Array/BasicProblems/MoveZeroToEnd.java
+++ src/Array/BasicProblems/MoveZeroToEnd.java
@@ -1,29 +1,64 @@
 package Array.BasicProblems;
 
 //Brute Force Approach
-public class MoveZeroToEnd {
-    static void moveZero(int arr[], int n){
-        int temp[]=new int[n];                   //creating a temp array to store non zero elements of array
-        int index=0;                            // count of non zero elements
-        for(int i=0; i<n; i++){                 // traversing the array to store non-zero into temp array
-            if(arr[i]!= 0)
-                temp[index++] = arr[i];            // non zero elements stored
+// public class MoveZeroToEnd {
+//     static void moveZero(int arr[], int n){
+//         int temp[]=new int[n];                   //creating a temp array to store non zero elements of array
+//         int index=0;                            // count of non zero elements
+//         for(int i=0; i<n; i++){                 // traversing the array to store non-zero into temp array
+//             if(arr[i]!= 0)
+//                 temp[index++] = arr[i];            // non zero elements stored
+//             }
+//             for(int i=0; i<index; i++){             //Running a loop from 0--index
+//                 arr[i]=temp[i];                      //to copy temp arr to original arr
+//               }
+//             for(int i=index; i<n; i++){          //traversing once again from index-n
+//                 arr[i]=0;                      // to place zeroes ata last
+//             }
+//     }
+
+    /*The time complexcity of this will be (i) first loop-O(n)
+                                         (ii)sexond loop-O(x)   suppose there are x no of non zero elements
+                                         (iii)third loop-O(n-x)
+                                          TC=O(n)+O(x)+O(n-x)=O(2n)
+
+   Space complexcity is O(n) as we are using temp arr of size n  */
+
+
+//Optimized Approach [Using two pointer approach]
+
+      public class MoveZeroToEnd {
+        static int[] moveZero(int arr[], int n){
+            int j=-1;                                //if 0 is not present in the whole arr
+            
+            for(int i=0; i<n; i++){
+                if(arr[i]==0){                      //searching for the first occurance of 0
+                    j=i;                            //as the 0 enountered in the arr assign j to 0
+                    break;                          // break the loop as we got the first occurance of 0
+                }
             }
-            for(int i=0; i<index; i++){             //Running a loop from 0--index
-                arr[i]=temp[i];                      //to copy temp arr to original arr
-              }
-            for(int i=index; i<n; i++){          //traversing once again from index-n
-                arr[i]=0;                      // to place zeroes ata last
+                if(j==-1){                          // if 0 is not find
+                    return arr;
+                }
+                for(int i=j+1; i<n; i++){              //traversing the whole arr after first occurance of 0 
+                    if(arr[i]!=0){
+                        //swap arr,i,j
+                        int temp=arr[i];               //as soon as non zero elements encountred swap it with 0
+                        arr[i]=arr[j];                 //increment j pointer i.e j will always be at 0
+                        arr[j]=temp;
+                        j++;
+                    }
+                }
+                return arr;
             }
-    }
 
-    //The time complexcity of this will be (i) first loop-O(n)
-                                         //(ii)sexond loop-O(x)   suppose there are x no of non zero elements
-                                         //(iii)third loop-O(n-x)
-                                         // TC=O(n)+O(x)+O(n-x)=O(2n)
-
-   //Space complexcity is O(n) as we are using temp arr of size n
-
+       /*       Time complexcity of this code will be (i) for first loop - O(x) i.e suppose 0 as x
+                                                  (ii) for second loop - O(n-x)
+                                                  TC = O(x)+O(n-x) = O(n);  
+             
+             Space Complexity is optimized in this as we are using only extra space to swap the
+             arr so SC will be O(1) */                                     
+        
     public static void main(String[] args){
         int arr[]={1,0,2,3,2,0,0,4,5,6};
         int n=arr.length;
---
+++ src/Array/BasicProblems/RemoveDuplicates.java
@@ -0,0 +1,82 @@
+package Array.BasicProblems;
+
+/*Optimal Approach - Using two pionter Approach*/
+/*tc will be O(n) for 1 pass */
+/*SC will be O(1) */
+public class RemoveDuplicates {
+   static int removeDuplicates(int[] arr, int n){
+    int i = 0;                               //place first pointer at 0 index
+    for(int j=1; j<n; j++){                  //place second pointer at i+1 index
+        if(arr[i]!=arr[j]){                  // if i!=j increment i and update the value of i=j
+         i++;
+           arr[i]=arr[j];
+        }
+    }
+    return i+1;                             //return the total no of elements as i will 3 but total no of elements will be 4
+}
+         public static void main(String[] args){
+           int arr[]={0,0,3,3,5,6};
+           int n=arr.length;
+           int result= removeDuplicates(arr, n);
+           for(int i=0; i<result; i++){
+           System.out.print(arr[i] + " ");
+        }
+     }
+   }
+
+
+/*If set is given & have to return the set only */
+/*The tc of this is 
+                    (i) O(N) for first loop as it will iterate through 0-n to find unique elements
+   The Sc of this is O(n) as set needs to add n elements */
+
+  /*  public class RemoveDuplicates{
+   public static Set<Integer>removeDuplicates(Integer[] arr){
+      HashSet<Integer>s=new HashSet<>();
+      
+      for(int i:arr){
+         s.add(i);
+      }
+      return s;
+   }
+
+   public static void main(String[] args){
+    Integer[] arr={0,0,3,3,5,6};
+    HashSet<Integer>result= (HashSet<Integer>) removeDuplicates(arr);
+      
+        System.out.print(result);
+    }
+   }                                              */
+
+
+
+/*If int is given brute force approach include creating a set and returning integer */
+
+// public class RemoveDuplicates{
+//    static int removeDuplicates(int arr[], int n){
+//       HashSet<Integer>s=new HashSet<>();                         //Initialize a hashset
+                                                           
+//       for(int i:arr){                                           //Interate through arr
+//          s.add(i);                     //add elements to set as set doesn,t contain duplicates it will add uniqueEle
+//       }
+
+
+//       int uniquesElements=s.size();                //store the size of unique elents
+//       int index=0;                                 //now place the pointer on actual arr and iterate the set
+//       for(int i:s){                                  
+//          arr[index]=i;                             //add elements back to arr
+//          index++;
+//       }
+//       return uniquesElements;                     
+
+//    }
+
+//    public static void main(String[] args){
+//               int arr[]={3,0,3,0,5,3,5,6,4};
+//               int n=arr.length;
+//              int result = removeDuplicates(arr, n);
+//              for(int i=0; i<result; i++)
+//             System.out.print(arr[i] + " ");
+             
+//            }
+//         }
